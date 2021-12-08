a4e5d2d1861148ccedd01eb27f31cdecb73bfadf
SVSathwick <sathwick.sv@gmail.com>
2021-12-08 08:39:45 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,41 @@
+# x = int(1)
+# print(x)
+# print(type(x))
+
+# y=int(2.8)
+# print(y)
+# print(type(y))
+
+# x=float(1)
+# print(x)
+# print(type(x))
+
+# y=float(3)
+# print(y)
+# print(y*y)
+
+# z='1.222'
+# print(z)
+# print(type(z))
+
+name = 'Venkata Sathwick'
+print(name)
+print(type(name)) 
+#test = int(name) #type error since name is not a full numner
+name=123
+print(name)
+print(type(name))
+
+salary='27000'
+print(int(salary))
+
+hikePercentage = '12.5'
+#print(int(hikePercentage)) #type error
+print(float(hikePercentage)) #prints 12.5
+print(int(float(hikePercentage))) #prints 12
+print(str(float(hikePercentage))) #prints 12.5
+
+id=1234
+print(id)
+print(type(id))
+
